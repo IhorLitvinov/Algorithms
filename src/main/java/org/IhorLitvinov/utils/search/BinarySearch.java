7043e3c565316421c1a1d9baee9d058bc0317550
Ihor Litvinov <ig.1372@gmail.com>
2016-01-13 14:52:55 +0200

    Abstract Binary Search algorithm has been added to repository

---
+++ src/main/java/org/IhorLitvinov/utils/search/BinarySearch.java
@@ -0,0 +1,63 @@
+package org.IhorLitvinov.utils.search;
+
+import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
+
+public class BinarySearch {
+    public enum BinarySearchMode {
+        BIGGEST,
+        LEAST
+    }
+
+    /**
+     * This method used for finding index of the element
+     * that satisfies the conditions using binary search
+     *
+     * @param compareElementWithIndexToRequired returns 0 if the element
+     *                                          at the specified index fits
+     *                                          the desired item, negative integer
+     *                                          if it is less the desired item
+     *                                          and positive integer if it is bigger
+     * @param mode                              the method will return
+     *                                          biggest index that fits or least
+     * @return index of the item that satisfies the conditions or -1
+     */
+    public int find(
+            IntUnaryOperator compareElementWithIndexToRequired,
+            BinarySearchMode mode,
+            int start,
+            int end
+    ) {
+
+        IntPredicate indexFit = index -> compareElementWithIndexToRequired
+                .applyAsInt(index) == 0;
+
+        IntPredicate needToMoveStart;
+        if (mode == BinarySearchMode.LEAST) {
+            needToMoveStart = middle -> compareElementWithIndexToRequired
+                    .applyAsInt(middle) < 0;
+        } else {
+            needToMoveStart = middle -> compareElementWithIndexToRequired
+                    .applyAsInt(middle) <= 0;
+        }
+
+        int middle = start + (end - start) / 2;
+        while (start != middle) {
+
+            if (needToMoveStart.test(middle)) {
+                start = middle;
+            } else {
+                end = middle;
+            }
+
+            middle = start + (end - start) / 2;
+        }
+
+        if (indexFit.test(middle)) {
+            return middle;
+        } else if (end > middle && indexFit.test(middle + 1)) {
+            return middle + 1;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/org/IhorLitvinov/utils/search/BinarySearchTest.java
@@ -0,0 +1,131 @@
+package org.IhorLitvinov.utils.search;
+
+import org.IhorLitvinov.utils.search.BinarySearch.BinarySearchMode;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.function.IntUnaryOperator;
+
+public class BinarySearchTest {
+    private BinarySearch binarySearch = new BinarySearch();
+
+    private int[] array = {0, 1, 3, 5, 6, 7, 8, 9, 11, 12, 14};
+
+    private int[] elseArray = {0, 1, 3, 15, 15, 15, 15, 19, 161, 412, 614};
+
+    private IntUnaryOperator isMore4andLess10 = index -> {
+        if (array[index] > 10) {
+            return 1;
+        } else if (array[index] < 4) {
+            return -1;
+        }
+        return 0;
+    };
+
+    private IntUnaryOperator elseIsMore4andLess10 = index -> {
+        if (elseArray[index] > 10) {
+            return 1;
+        } else if (elseArray[index] < 4) {
+            return -1;
+        }
+        return 0;
+    };
+
+    private IntUnaryOperator isEquals15 = index -> {
+        if (elseArray[index] > 15) {
+            return 1;
+        } else if (elseArray[index] < 15) {
+            return -1;
+        }
+        return 0;
+    };
+
+    @Test
+    public void testFindBiggestValueMore4andLess10() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.BIGGEST, 0, 10);
+
+        int expectedResultIndex = 7;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindLeastValueMore4andLess10() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.LEAST, 0, 10);
+        int expectedResultIndex = 3;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindValueMore4andLess10Between0and2indexes() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.LEAST, 0, 2);
+        int expectedResultIndex = -1;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindValueMore4andLess10Between8and10indexes() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.LEAST, 8, 10);
+        int expectedResultIndex = -1;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindValueMore4andLess10Between2and2indexes() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.LEAST, 2, 2);
+        int expectedResultIndex = -1;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindLeastValueMore4andLess10Between8and8indexes() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.LEAST, 6, 6);
+        int expectedResultIndex = 6;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindBiggestValueMore4andLess10Between8and8indexes() {
+        int resultIndex = binarySearch
+                .find(isMore4andLess10, BinarySearchMode.BIGGEST, 6, 6);
+        int expectedResultIndex = 6;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindLeastValueMore4andLess10WhereItIsNot() {
+        int resultIndex = binarySearch
+                .find(elseIsMore4andLess10, BinarySearchMode.LEAST, 0, 10);
+        int expectedResultIndex = -1;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindBiggestValueMore4andLess10WhereItIsNot() {
+        int resultIndex = binarySearch
+                .find(elseIsMore4andLess10, BinarySearchMode.BIGGEST, 0, 10);
+        int expectedResultIndex = -1;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindLeastValueIsEquals15WithDuplicates() {
+        int resultIndex = binarySearch
+                .find(isEquals15, BinarySearchMode.LEAST, 0, 10);
+        int expectedResultIndex = 3;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+
+    @Test
+    public void testFindBiggestValueIsEquals15WithDuplicates() {
+        int resultIndex = binarySearch
+                .find(isEquals15, BinarySearchMode.BIGGEST, 0, 10);
+        int expectedResultIndex = 6;
+        Assert.assertEquals(expectedResultIndex, resultIndex);
+    }
+}
